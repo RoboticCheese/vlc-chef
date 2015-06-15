b2efaefa84342af7c365934e03e5d5ae259e26fa
Jonathan Hartman <j@p4nt5.com>
2015-06-14 23:40:33 -0700

    Initial commit; freshly generated

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+Vlc Cookbook CHANGELOG
+======================
+
+v0.0.1 (2015-06-14)
+-------------------
+- Development started
---
+++ metadata.rb
@@ -0,0 +1,11 @@
+# Encoding: UTF-8
+#
+# rubocop:disable SingleSpaceBeforeFirstArg
+name             'vlc'
+maintainer       'Jonathan Hartman'
+maintainer_email 'j@p4nt5.com'
+license          'apache2'
+description      'Installs/Configures vlc'
+long_description 'Installs/Configures vlc'
+version          '0.0.1'
+# rubocop:enable SingleSpaceBeforeFirstArg
